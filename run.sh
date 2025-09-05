e2bf2afdd41739fb67c06778f44b4d339fe15f57
Eric Cornelissen <ericornelissen@gmail.com>
2025-09-05 13:50:51 +0200

    improve/fix debug logs

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ PROJECT=$1
 VULN_TYPES_RAW=$2
 TIMEOUT_TOTAL=$3
 
-echo '::debug::GOT:'
-echo "::debug::  PROJECT: $PROJECT"
-echo "::debug::  VULN_TYPES_RAW: $VULN_TYPES_RAW"
-echo "::debug::  TIMEOUT_TOTAL: $TIMEOUT_TOTAL"
+awk '{print "::debug::"$0}' <<<'GOT:'
+awk '{print "::debug::"$0}' <<<"PROJECT:        $PROJECT"
+awk '{print "::debug::"$0}' <<<"VULN_TYPES_RAW: $VULN_TYPES_RAW"
+awk '{print "::debug::"$0}' <<<"TIMEOUT_TOTAL:  $TIMEOUT_TOTAL"
 
 
 VULN_TYPES=$(echo "$VULN_TYPES_RAW" | tr ',' '\n' | sed -E 's/^[[:space:]]+|[[:space:]]+$//g' | sed '/^$/d')
@@ -17,16 +17,17 @@ VULN_TYPES_COUNT=$(echo "$VULN_TYPES" | wc -l)
 TIMEOUT=$((TIMEOUT_TOTAL/VULN_TYPES_COUNT))
 
 echo '::debug::DERIVED:'
-echo "::debug::  VULN_TYPES: $VULN_TYPES"
-echo "::debug::  VULN_TYPES_COUNT: $VULN_TYPES_COUNT"
-echo "::debug::  TIMEOUT: $TIMEOUT"
+awk '{print "::debug::"$0}' <<<'DERIVED:'
+awk '{print "::debug::"$0}' <<<"VULN_TYPES:       $VULN_TYPES"
+awk '{print "::debug::"$0}' <<<"VULN_TYPES_COUNT: $VULN_TYPES_COUNT"
+awk '{print "::debug::"$0}' <<<"TIMEOUT:          $TIMEOUT"
 
 
 while IFS= read -r VULN_TYPE; do
-	echo '::debug::Running ODGen:'
-	echo "::debug::  PROJECT: $PROJECT"
-	echo "::debug::  VULN_TYPE: $VULN_TYPE"
-	echo "::debug::  TIMEOUT: $TIMEOUT"
+	awk '{print "::debug::"$0}' <<<'Running ODGen with:'
+	awk '{print "::debug::"$0}' <<<"PROJECT:   $PROJECT"
+	awk '{print "::debug::"$0}' <<<"VULN_TYPE: $VULN_TYPE"
+	awk '{print "::debug::"$0}' <<<"TIMEOUT:   $TIMEOUT"
 
 	python3 /app/ODGen/odgen.py \
 		"$PROJECT" \
